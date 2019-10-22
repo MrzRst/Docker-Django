1fb4938b921ef6249fb0d1c9908426171ec00add
Marzieh Rostami <51113356+MrzRst@users.noreply.github.com>
2019-10-22 18:35:39 +0330

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Step 3 – Create A Django Application
+cd /var/www
+django-admin startproject django_app
+cd django_app
+python3 manage.py migrate
+
+
+#Step 4 – Create Super User
+python3 manage.py createsuperuser
+
+#Step 5 – Run Django Application
+sed "s/ALLOWED_HOSTS = []/ALLOWED_HOSTS = ['127.0.0.1']/g" django_app/settings.py
+
+python3 manage.py runserver 0.0.0.0:8010
